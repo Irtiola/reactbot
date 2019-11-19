54b05e613c60e8e9c6f8ef32b94106033fe11cb8
Irtiola <irtiola@gmail.com>
2019-11-18 17:01:34 -0800

    DialogFlow-module-Authentication-setup-part-2

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -1,3 +1,9 @@
+const dialogFlow = require("dialogflow");
+const config = require("../config/keys");
+
+const sessionClient = new dialogFlow.sessionClient;
+const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID)
+
 module.exports = app => {
 
     app.get('/', (req, res) => {
@@ -5,6 +11,34 @@ module.exports = app => {
     });
 
     app.post('/api/df_text_query', (req, res) => {
+        // The text query request.
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                text: {
+                    // The query to send to the dialogflow agent
+                    text: req.body.text,
+                    // The language used by the client (en-US)
+                    languageCode: config.dialogFlowSessionLanguageCode,
+                },
+            },
+        };
+        sessionClient
+            .detectIntent(request)
+            .then(responses => {
+                console.log('Detected intent');
+                const result = responses[0].queryResult;
+                console.log(`  Query: ${result.queryText}`);
+                console.log(`  Response: ${result.fulfillmentText}`);
+                if (result.intent) {
+                    console.log(`  Intent: ${result.intent.displayName}`);
+                } else {
+                    console.log(`  No intent matched.`);
+                }
+            })
+            .catch(err => {
+                console.error('ERROR:', err);
+            });
         res.send({ 'do': 'text query' })
     });
 
